da763872b3246c97cddf7cf63114b91978b8469b
rihan010 <ibrhimrihan000@gmail.comm>
2023-03-31 00:04:25 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
+import Card from './components/Cards/Card';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container mx-auto">
       <Header></Header>
+      <hr />
+      <div className='row'>
+        <div className='col-md-8'>
+<Card></Card>
+        </div>
+        <div className='col-md-4 '>
+<Sidebar></Sidebar>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,17 +5,11 @@ import user from '../../images/user-1.png'
 const Header = () => {
     return (
         <div>
-            <nav className='mt-5'>
-                <div className='container flex justify-around items-center'>
-                    <div>
-                        <h2 className='text-2xl font-bold'>Knowledge Cafe</h2>
-                    </div>
-                    <img className='w-12' src={user} alt="" />
-
+            <nav className="navbar bg-light">
+                <div className="container-fluid">
+                   <h1>Knowledge Cafe</h1>
+                    <img src={user} alt="" />
                 </div>
-
-
-
             </nav>
         </div>
     );
