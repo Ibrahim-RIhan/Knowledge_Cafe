24645a914cbc48fdc0a027b6833d0cb7f1369289
rihan010 <ibrhimrihan000@gmail.comm>
2023-03-31 00:55:37 +0600

    fontawesome added

--- src/components/SingleCard/SingleCard.js
+++ src/components/SingleCard/SingleCard.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './SingleCard.css'
 import user from '../../images/user-1.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { faBookBookmark, } from '@fortawesome/free-solid-svg-icons'
 
 const SingleCard = (props) => {
     const { img, name, title, time, date } = props.data;
@@ -8,24 +10,23 @@ const SingleCard = (props) => {
         <div className='SingleCard'>
             <div class="card mb-3">
                 <img className='img-fluid' src={img} alt="" />
-                <div class="card-body">
+                <div class="card-body lh-base">
                     <div className='d-flex justify-content-between'>
-                        <div className='d-flex align-items-center gap-2'>
+                        <div className='d-flex align-items-center'>
                             <img src={user} alt="" />
-                            <div >
+                            <div className='lh-1'>
                                 <h6 className='fw-bold'>{name}</h6>
-                                <p>{date}</p>
+                                <p className='text-secondary ms-3'>{date}</p>
                             </div>
                         </div>
-                        <div>
-                            <p>{time}</p>
-
-                        </div>
+                        
+                            <p>{time} <FontAwesomeIcon icon={faBookBookmark} /></p>
+                       
                     </div>
-                    <div className='text-start'>
-                    <h1>{title}</h1>
-                    <p>#Hashtag</p>
-                    <button>Mark as read</button>
+                    <div className='text-start my-3'>
+                    <h1 className='fs-2 text fw-bold'>{title}</h1>
+                    <p className='text-secondary'>#Hashtag    #hashtag</p>
+                    <a href="3" >Mark as read</a>
                 </div>
                 </div>
                
