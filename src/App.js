547d03e780d6ad27db11ccc9e6f141fc34f850fc
rihan010 <ibrhimrihan000@gmail.comm>
2023-03-31 16:25:21 +0600

    toast added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from './components/Header/Header';
 import Card from './components/Cards/Card';
 import Sidebar from './components/Sidebar/Sidebar';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 
 
 function App() {
   const [bookmarkN0, setbookmarkN0] = useState(0);
   const [timeSpend, setTimeSpend] = useState(0);
-  const [bookmarks ,setBookmarks] =useState([])
+  const [bookmarks, setBookmarks] = useState([])
   const miniutesRead = (time) => {
     setTimeSpend(timeSpend + time)
   }
   const handleBookmark = (title) => {
     setbookmarkN0(bookmarkN0 + 1);
     if (bookmarks.includes(title)) {
-      alert('already bookmark')
+      toast.error('Bookmark Already Added', {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        });
     } else {
       setBookmarks([...bookmarks, title]);
+      toast('🦄 Wow so easy!', {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        });
     }
 
 
@@ -29,7 +47,8 @@ function App() {
 
 
   return (
-    <div className="App container mx-auto">
+    <div className="App container mx-auto">\
+    <ToastContainer></ToastContainer>
       <Header></Header>
       <hr />
       <div className='row'>
@@ -42,7 +61,7 @@ function App() {
           <Sidebar
             timeSpend={timeSpend}
             bookmarkN0={bookmarkN0}
-            bookmarks = {bookmarks}
+            bookmarks={bookmarks}
           ></Sidebar>
         </div>
       </div>
